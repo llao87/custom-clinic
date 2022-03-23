7da6c5829649fa93ae130d956466f89ff0ff7403
Dmitry Kragel <dmitry.kragel@gmail.com>
2022-03-23 16:51:21 +0300

    prefinal v0.2

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,24 +10,26 @@
 </head>
 
 <body>
+    <div class="page">
+        <div class="logo">
+            <a href="/" class="logo__link">
+                <img src="img/logo.svg" alt="Custom Clinic" class="logo__img">
+            </a>
+        </div>
 
-    <div class="logo">
-        <a href="/" class="logo__link">
-            <img src="img/logo.svg" alt="Custom Clinic" class="logo__img">
-        </a>
+        <div class="offer">
+            <a href="#" class="offer__text">
+                <span class="offer__text-row">Зачем пересаживать волосы </span>
+                <span class="offer__text-row">в России весной 2022 года</span>
+            </a>
+        </div>
+
+        <div class="coming-soon">Скоро <br> открытие</div>
+
+        <div class="copyrights">Центр эстетической медицины</div>
+
+        <script></script>
     </div>
-
-    <div class="offer">
-        <a href="#" class="offer__text">
-            <span class="offer__text-row">Зачем пересаживать волосы </span>
-            <span class="offer__text-row">в России весной 2022 года</span>
-        </a>
-    </div>
-
-    <div class="coming-soon">Скоро <br> открытие</div>
-
-    <div class="copyrights">Центр эстетической медицины</div>
-
-    <script></script>
 </body>
+
 </html>
