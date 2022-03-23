32b043806e3540b7dffaaf79e6a2d2caac49fc43
Dmitry Kragel <dmitry.kragel@gmail.com>
2022-03-23 11:47:02 +0300

    init

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Custom Clinic | Центр эстетической медицины</title>
+</head>
+
+<body>
+
+    <div class="logo">
+        <a href="/" class="logo__link">
+            <img src="img/logo.svg" alt="Custom Clinic" class="logo__img">
+        </a>
+    </div>
+
+    <div class="offer">
+        <p class="offer__text">
+            <span class="offer__text-row">Зачем пересаживать волосы </span>
+            <span class="offer__text-row">в России весной 2022 года</span>
+        </p>
+    </div>
+
+    <div class="coming-soon">Скоро открытие</div>
+
+    <div class="copyrights">Центр эстетической медицины</div>
+
+    <script></script>
+</body>
+</html>
